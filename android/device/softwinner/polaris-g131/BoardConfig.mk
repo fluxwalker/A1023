ff8e040ae15fef5b87ca097f90b8b64218a81ef2
fluxwalker <johnathan.a.ragazzo@gmail.com>
2015-05-25 05:43:00 -0400

    first commit

---
+++ android/device/softwinner/polaris-g131/BoardConfig.mk
@@ -0,0 +1,15 @@
+# BoardConfig.mk
+#
+# Product-specific compile-time definitions.
+#
+
+include device/softwinner/polaris-common/BoardConfigCommon.mk
+
+# image-related
+
+#BOARD_KERNEL_CMDLINE := 
+#BOARD_KERNEL_PAGESIZE :=
+#BOARD_BOOTIMAGE_PARTITION_SIZE :=
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE :=
+#BOARD_USERDATAIMAGE_PARTITION_SIZE :=
+BOARD_HAS_NO_SELECT_BUTTON := true
